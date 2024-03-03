6a2bbdbad9a297ae34414764090300144194a071
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-03-03 00:23:35 -0300

    feat: set floating ui globally on frontend

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,9 +5,13 @@
  */
 
 import axios from 'axios';
+import * as floatingUi from "@floating-ui/dom";
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.floating = floatingUi;
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
