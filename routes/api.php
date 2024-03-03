5678a4014f53aaca72176ee123ed1cd4a79286b4
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-03-03 00:25:33 -0300

    feat: api routes - create new user api endpoint - logout api endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\SessionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,4 +22,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::controller(SessionController::class)->group(function() {
     Route::post('/login', "store")->name("session.store");
+});
+
+Route::middleware(['auth', 'auth.session'])->group(function() {
+    Route::controller(UserController::class)->group(function() {
+        Route::post("/user/new", "store")->name("user.store");
+    });
+
+    Route::controller(SessionController::class)->group(function() {
+        Route::get('/logout', "destroy")->name("session.logout");
+    });
 });
