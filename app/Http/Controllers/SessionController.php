f131b3d32d7f27d5e508f088574214ae0e203943
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-03-03 00:25:06 -0300

    feat: session controller - finish login logic - logout logic

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SessionController extends Controller
 {
     public function login() {
-        return view ("login");
+        return view("login");
     }
 
     public function store(Request $request) {
@@ -19,7 +21,33 @@ class SessionController extends Controller
             "nickname.required" => "Digite seu nickname para logar.",
             "nickname.string" => "Digite um nickname válido.",
             "password.required" => "Digite sua senha para logar.",
-            "password.min" => "A senha deve ser maior que :min."
+            "password.min" => "A senha deve ser maior que :min caracteres."
         ]);
+
+        $credentials = $request->only("nickname", "password");
+
+        $user = User::firstWhere("nickname", $credentials["nickname"]);
+
+        if (!$user) {
+            return redirect()->route("session.login")->withErrors(["error" => "Nickname ou senha incorreto(s)."]);
+        }
+
+        $password_matches = password_verify($credentials["password"], $user->password);
+        
+        if (!$password_matches) {
+            return redirect()->route("session.login")->withErrors(["error" => "Nickname ou senha incorreto(s)."]);
+        }
+
+        if ($user->active == false) {
+            return redirect()->route("user.create")->withErrors(["error" => "Usuário com nickname já existe."]);
+        }
+
+        Auth::login($user, true);
+        return redirect()->route("home");
+    }
+
+    public function destroy() {
+        Auth::logout();
+        return redirect()->route("session.login");
     }
 }
