30aeed3ea74d5613bf57c98c5985cf44b69cda42
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-03-09 02:58:28 -0300

    refactor: sidebar.blade.php - remove unecessary php code

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -2,9 +2,6 @@
 use App\View\Components\Types\AccordionLink;
 use App\Enums\RoleEnum;
 
-$registros_icon = asset("images/submenu-historico.png");
-$registros_href = "/registros";
-
 $destaques_links = [
     new AccordionLink ("Ver todos", "/"),
     new AccordionLink ("Adicionar destaque", "/e", [RoleEnum::Gestor]),
@@ -12,7 +9,7 @@ $destaques_links = [
 $destaques_icon = asset("images/submenu-destaque.png");
 
 $membros_links = [
-    new AccordionLink ("Adicionar membro", "/d", [RoleEnum::Mestre, RoleEnum::Gestor]),
+    new AccordionLink ("Adicionar membro", route("user.create"), [RoleEnum::Mestre, RoleEnum::Gestor]),
     new AccordionLink ("Lista de membros", "/c"),
     new AccordionLink ("Gerenciar pontos", "/b", [RoleEnum::Gestor]),
     new AccordionLink ("Checar pontos", "/a"),
@@ -22,16 +19,18 @@ $membros_icon = asset("images/submenu-membros.png");
 @endphp
 
 <aside class="w-96 h-auto bg-gray-700 flex flex-col">
-    <div class="sidebar-separator">
+    <x-sidebar-link title="Início" href="{{route('home')}}" icon="{{asset('images/submenu-home.png')}}" />
+    
+    {{-- <div class="sidebar-separator">
         Blog
-    </div>
+    </div> --}}
 
     {{-- ////////////////////////////// --}}
     
     <div class="sidebar-separator">
         Administração
     </div>
-    <x-sidebar-link title="Registros" :href="$registros_href" :icon="$registros_icon" />
+    <x-sidebar-link title="Registros" href="/registros" icon="{{asset('images/submenu-historico.png')}}" />
     <x-sidebar-accordion title="Destaques" :links="$destaques_links" :icon="$destaques_icon" :user-role="$user->role" />
     <x-sidebar-accordion title="Membros" :links="$membros_links" :icon="$membros_icon" :user-role="$user->role" />
 </aside>
